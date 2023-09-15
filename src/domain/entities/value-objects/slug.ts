6854404be843ec770b719e533b48667d4dee770c
thiago da silva <thg021@gmail.com>
2023-09-15 08:26:17 -0300

    chore: configurado o eslint

--- src/domain/entities/value-objects/slug.ts
+++ src/domain/entities/value-objects/slug.ts
@@ -1,30 +1,30 @@
 export class Slug {
-  public value: string;
+  public value: string
 
   constructor(value: string) {
-    this.value = value;
+    this.value = value
   }
 
   /**
    * Receives a string and normalize it as a slug.
    *
-   *@example 
+   *@example
    *```text
    * An example title" => "an-example-title
    *```
    *
    * @param text {string}
-  */
+   */
   static createFromText(text: string): Slug {
     const slugText = text
-      .normalize("NFKD")
+      .normalize('NFKD')
       .toLowerCase()
       .trim()
-      .replace(/\s+/g, "-")
-      .replace(/[^\w-]+/g, "")
-      .replace(/_/g, "-")
-      .replace(/--+/g, "-")
-      .replace(/-$/g, "");
-    return new Slug(slugText);
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
+    return new Slug(slugText)
   }
 }
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,20 +1,18 @@
-import { expect, test } from "vitest";
-import { AnswerQuestionUseCase } from "./answer-question";
-import { AnswersRepository } from "../repositories/answers-repository";
-import { Answer } from "../entities/answer";
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
 
 const fakeAnswerRepository: AnswersRepository = {
-  create: async (answer: Answer) => {
-    return;
-  },
-};
-test("create an answer", async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+  create: async (answer: Answer) => {},
+}
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
   const answer = await answerQuestion.execute({
-    content: "content",
-    instructorId: "instructorId",
-    questionId: "questionId",
-  });
+    content: 'content',
+    instructorId: 'instructorId',
+    questionId: 'questionId',
+  })
 
-  expect(answer.content).toEqual("content");
-});
+  expect(answer.content).toEqual('content')
+})
