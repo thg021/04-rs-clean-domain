8fb715517c221ae5f53a911ba93c51c685dfcc5d
thiago da silva <thg021@gmail.com>
2023-09-18 09:29:30 -0300

    feat: gerando dados ficticios

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,4 +1,3 @@
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/question-repository'
 
